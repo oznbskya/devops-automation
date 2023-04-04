ed2060e7f43e993374ae9d331150b346bea8fb82
ozanbskya <ozanbskya@gmail.com>
2023-04-04 14:13:09 +0300

    comment added

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DevopsIntegrationApplication {
 
+	// this is comment
 	@GetMapping
 	public String message(){
 		return "welcome to javatechie";
